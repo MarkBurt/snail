83733e6c0b9d4e4505c65320ab7cdf9afe400dd0
MarkBurt <wangye175005@163.com>
2020-02-17 16:15:58 +0800

    init Mysql Config

---
+++ snail_behavior/src/main/java/com/mark/behavior/config/MysqlConfig.java
@@ -0,0 +1,19 @@
+package com.mark.behavior.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description :  mysql 的配置
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/17$ 下午 04:15$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/17$ 下午 04:15$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+
+@Configuration
+@ComponentScan("com.mark.common.core")
+public class MysqlConfig {
+}
