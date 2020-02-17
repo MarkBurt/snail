19c4bfc11ff5c00c5202913289c0eb38505656b6
MarkBurt <wangye175005@163.com>
2020-02-17 16:18:19 +0800

    init Zookeeper Config.

---
+++ snail_behavior/src/main/java/com/mark/behavior/config/ZookeeperConfig.java
@@ -0,0 +1,19 @@
+package com.mark.behavior.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description :  Zookeeper的配置
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/17$ 下午 04:16$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/17$ 下午 04:16$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+
+@Configuration
+@ComponentScan("com.mark.common.zookeeper")
+public class ZookeeperConfig {
+}
