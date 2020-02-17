3add0436c455d61a90a497ee7647ab17140716c9
MarkBurt <wangye175005@163.com>
2020-02-17 16:02:26 +0800

    increase User Action and init article start class.

--- snail_behavior/src/main/java/com/mark/behavior/BehaviorJarApplication.java
+++ snail_behavior/src/main/java/com/mark/behavior/BehaviorJarApplication.java
@@ -1,23 +1,22 @@
-package com.mark.article;
+package com.mark.behavior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Description :  Article启动类
+ * @Description :  用户行为服务启动
  * @Author :  Markburt
- * @CreateDate :  2020/2/17$ 上午 11:25$
+ * @CreateDate :  2020/2/17$ 下午 03:58$
  * @UpdateUser :  Markburt
- * @UpdateDate :  2020/2/17$ 上午 11:25$
+ * @UpdateDate :  2020/2/17$ 下午 03:58$
  * @UpdateRemark :  Project Build
  * @Version :  1.0
  */
 
-
 @SpringBootApplication
-public class ArticleJarApplication {
-
+public class BehaviorJarApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ArticleJarApplication.class,args);
+        SpringApplication.run(BehaviorJarApplication.class,args);
     }
+
 }
