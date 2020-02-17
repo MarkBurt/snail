6b81d50c9292a56bb49af890c001096c177e6d99
MarkBurt <wangye175005@163.com>
2020-02-17 12:16:12 +0800

    文章接口

--- snail_apis/src/main/java/com/mark/article/apis/ArticleHomeControllerApi.java
+++ snail_apis/src/main/java/com/mark/article/apis/ArticleHomeControllerApi.java
@@ -3,6 +3,15 @@ package com.mark.article.apis;
 import com.mark.model.article.dtos.ArticleHomeDto;
 import com.mark.model.common.dtos.ResponseResult;
 
+/**
+ * @Description :  文章接口
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/17$ 下午 12:06$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/17$ 下午 12:06$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
 public interface ArticleHomeControllerApi {
 
     /**
