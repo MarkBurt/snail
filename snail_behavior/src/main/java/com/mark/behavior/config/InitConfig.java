0035a537e9f5b2a377767cc1b6a1971b17a84942
MarkBurt <wangye175005@163.com>
2020-02-17 16:14:30 +0800

    init config

---
+++ snail_behavior/src/main/java/com/mark/behavior/config/InitConfig.java
@@ -0,0 +1,19 @@
+package com.mark.behavior.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description :  初始化配置信息
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/17$ 下午 04:13$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/17$ 下午 04:13$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+
+@Configuration
+@ComponentScan({"com.mark.common.common.init","com.mark.common.kafka"})
+public class InitConfig {
+}
