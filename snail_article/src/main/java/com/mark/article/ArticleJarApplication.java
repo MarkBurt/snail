23e30bddbd94e4ed7b9c9d7f6294041ca174020d
MarkBurt <wangye175005@163.com>
2020-02-17 11:27:16 +0800

    文章模块启动

---
+++ snail_article/src/main/java/com/mark/article/ArticleJarApplication.java
@@ -0,0 +1,23 @@
+package com.mark.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description :  Article启动类
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/17$ 上午 11:25$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/17$ 上午 11:25$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+
+
+@SpringBootApplication
+public class ArticleJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleJarApplication.class);
+    }
+}
