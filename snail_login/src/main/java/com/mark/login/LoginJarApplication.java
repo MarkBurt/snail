d70187ed157592371340321bd5239cc0a8ff54e5
MarkBurt <wangye175005@163.com>
2020-02-22 23:30:39 +0800

    Modify the startup class configuration

--- snail_login/src/main/java/com/mark/login/LoginJarApplication.java
+++ snail_login/src/main/java/com/mark/login/LoginJarApplication.java
@@ -4,18 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Description :  用户登录服务
+ * @Description :  java类作用描述
  * @Author :  Markburt
- * @CreateDate :  2020/2/18$ 下午 01:52$
+ * @CreateDate :  2020/2/22$ 下午 11:29$
  * @UpdateUser :  Markburt
- * @UpdateDate :  2020/2/18$ 下午 01:52$
+ * @UpdateDate :  2020/2/22$ 下午 11:29$
  * @UpdateRemark :  Project Build
  * @Version :  1.0
  */
-
 @SpringBootApplication
-public class UserJarApplication {
+public class LoginJarApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserJarApplication.class,args);
+        SpringApplication.run(LoginJarApplication.class,args);
     }
 }
