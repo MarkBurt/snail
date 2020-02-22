86c8d0d5ab452653548f41d82123e531ade5fd8f
MarkBurt <wangye175005@163.com>
2020-02-22 23:31:18 +0800

    Build the launch class configuration

---
+++ snail_user/src/main/java/com/mark/login/UserJarApplication.java
@@ -0,0 +1,20 @@
+package com.mark.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description :  java类作用描述
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/22$ 下午 11:26$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/22$ 下午 11:26$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+@SpringBootApplication
+public class UserJarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserJarApplication.class,args);
+    }
+}
