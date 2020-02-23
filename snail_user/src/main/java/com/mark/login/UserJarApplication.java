0eccbedad105a32f8f6b07135ab3603963601959
MarkBurt <wangye175005@163.com>
2020-02-23 18:36:45 +0800

    1.of change that you're committing 选择改动类型 (<type>)
    2.What is the scope of this change (e.g. component or file name)? 填写改动范围 (<scope>)
    snail_user
    3.Write a short, imperative tense description of the change: 写一个精简的描述 (<subject>)
    用户关注和取消关注
    4.Provide a longer description of the change: (press enter to skip) 对于改动写一段长描述 (<body>)
    完成关于用户关注和取消关注功能
    
    5.Are there any breaking changes? (y/n) 是破坏性修改吗？默认n (<footer>)
    n
    
    6.Does this change affect any openreve issues? (y/n) 改动修复了哪个问题？默认n (<footer>)
    n

--- snail_user/src/main/java/com/mark/login/UserJarApplication.java
+++ snail_user/src/main/java/com/mark/login/UserJarApplication.java
@@ -15,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserJarApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(UserJarApplication.class,args);
     }
 }
