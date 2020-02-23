d03631b3bf9e8b76f61b99c98c9b2720be453219
MarkBurt <wangye175005@163.com>
2020-02-23 13:34:06 +0800

    Initialization zookeeper configuration

---
+++ snail_user/src/main/java/com/mark/login/config/SequenceConfig.java
@@ -0,0 +1,18 @@
+package com.mark.login.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description :  zookeeper配置
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/23$ 下午 01:33$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/23$ 下午 01:33$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+@Configuration
+@ComponentScan("com.mark.common.zookeeper")
+public class SequenceConfig {
+}
