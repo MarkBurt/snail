54076c41c512dc606aede6b4dd13ab5fcac03e7f
MarkBurt <wangye175005@163.com>
2020-02-23 13:26:46 +0800

    Initialization configuration

---
+++ snail_user/src/main/java/com/mark/login/config/InitConfig.java
@@ -0,0 +1,18 @@
+package com.mark.login.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description :  java类作用描述
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/23$ 下午 01:22$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/23$ 下午 01:22$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+@Configuration
+@ComponentScan("com.mark.common.common.init")
+public class InitConfig {
+}
