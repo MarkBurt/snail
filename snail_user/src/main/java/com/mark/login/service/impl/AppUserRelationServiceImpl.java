1f0aed2eaee59d7e96ecc3396fd27b92182ed7a4
MarkBurt <wangye175005@163.com>
2020-02-23 13:46:48 +0800

    init User Service  implementation class

---
+++ snail_user/src/main/java/com/mark/login/service/impl/AppUserRelationServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mark.login.service.impl;
+
+import com.mark.login.service.AppUserRelationService;
+import com.mark.model.common.dtos.ResponseResult;
+import com.mark.model.user.dtos.UserRelationDto;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description :  java类作用描述
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/23$ 下午 01:45$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/23$ 下午 01:45$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+@Service
+@SuppressWarnings("all")
+public class AppUserRelationServiceImpl implements AppUserRelationService {
+
+
+    @Override
+    public ResponseResult follow(UserRelationDto dto) {
+        return null;
+    }
+}
