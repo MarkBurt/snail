8d3585a37ac632d8ad36d79268499ffef360b8a5
MarkBurt <wangye175005@163.com>
2020-02-23 13:29:55 +0800

    Initialization MysqlConfig configuration

---
+++ snail_user/src/main/java/com/mark/login/config/MysqlConfig.java
@@ -0,0 +1,18 @@
+package com.mark.login.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description :  java类作用描述
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/23$ 下午 01:27$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/23$ 下午 01:27$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+@Configuration
+@ComponentScan("com.mark.common.mysql.core")
+public class MysqlConfig {
+}
