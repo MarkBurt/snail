36658532add84a2b4669cb5154c0e9f4d14cc4dd
MarkBurt <wangye175005@163.com>
2020-02-23 17:46:34 +0800

    init

--- snail_model/src/main/java/com/mark/model/article/pojos/ApAuthor.java
+++ snail_model/src/main/java/com/mark/model/article/pojos/ApAuthor.java
@@ -10,10 +10,15 @@ public class ApAuthor {
     private Integer id;
     private String name;
     private Integer type;
-    // APP社交账号
+    /**
+     * APP社交账号
+     */
     private Long userId;
-    // 自媒体管理账号
+    /**
+     * 自媒体管理账号
+     */
     private Long wmUserId;
+
     @DateConvert("yyyyMMddHHmmss")
     private Date createdTime;
 }
