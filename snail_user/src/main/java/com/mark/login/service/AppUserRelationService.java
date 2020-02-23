881cc67646d4da0d59f3bc24d4100f1cae38bd2d
MarkBurt <wangye175005@163.com>
2020-02-23 13:43:29 +0800

    init User Service

---
+++ snail_user/src/main/java/com/mark/login/service/AppUserRelationService.java
@@ -0,0 +1,18 @@
+package com.mark.login.service;
+
+import com.mark.model.common.dtos.ResponseResult;
+import com.mark.model.user.dtos.UserRelationDto;
+
+/**
+ * @Description :  java类作用描述
+ * @Author :  Markburt
+ * @CreateDate :  2020/2/23$ 下午 01:40$
+ * @UpdateUser :  Markburt
+ * @UpdateDate :  2020/2/23$ 下午 01:40$
+ * @UpdateRemark :  Project Build
+ * @Version :  1.0
+ */
+public interface AppUserRelationService {
+
+
+}
