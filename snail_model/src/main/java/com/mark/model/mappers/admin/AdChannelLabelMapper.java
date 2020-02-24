276abeadb9bd101cfc975567dee226da52395e45
MarkBurt <wangye175005@163.com>
2020-02-24 14:08:45 +0800

    add comment

--- snail_model/src/main/java/com/mark/model/mappers/admin/AdChannelLabelMapper.java
+++ snail_model/src/main/java/com/mark/model/mappers/admin/AdChannelLabelMapper.java
@@ -2,18 +2,51 @@ package com.mark.model.mappers.admin;
 
 import com.mark.model.admin.pojos.AdChannelLabel;
 
+/**
+ * @author MAIBENBEN
+ */
 public interface AdChannelLabelMapper {
-    
+
+    /**
+     *
+     * @param id
+     * @return
+     */
     int deleteByPrimaryKey(Integer id);
 
+    /**
+     *
+     * @param record
+     * @return
+     */
     int insert(AdChannelLabel record);
 
+    /**
+     *
+     * @param record
+     * @return
+     */
     int insertSelective(AdChannelLabel record);
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     AdChannelLabel selectByPrimaryKey(Integer id);
 
+    /**
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKeySelective(AdChannelLabel record);
 
+    /**
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKey(AdChannelLabel record);
 
     /**
